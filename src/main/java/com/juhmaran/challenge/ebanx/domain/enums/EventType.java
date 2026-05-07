0fe882a97db6653e4e486616541e67bc3694f274
JuhMaran <julianemaran@gmail.com>
2026-05-07 15:11:22 -0300

    feat: add event type enum

---
+++ src/main/java/com/juhmaran/challenge/ebanx/domain/enums/EventType.java
@@ -0,0 +1,13 @@
+package com.juhmaran.challenge.ebanx.domain.enums;
+
+/**
+ * ebanx
+ *
+ * @author Juliane Maran
+ * @since 07/05/2026
+ */
+public enum EventType {
+  DEPOSIT,
+  WITHDRAW,
+  TRANSFER
+}
