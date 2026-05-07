c3c66648233211f4edc0d4e8c7d2eed61cdefbce
JuhMaran <julianemaran@gmail.com>
2026-05-07 13:41:46 -0300

    chore: initialize project with Spring Initializr

---
+++ src/main/java/com/juhmaran/challenge/ebanx/EbanxApplication.java
@@ -0,0 +1,13 @@
+package com.juhmaran.challenge.ebanx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juhmaran/challenge/ebanx/EbanxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhmaran.challenge.ebanx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbanxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
